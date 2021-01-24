c05b6b100901208dd95229f96c6e9d5cb43274f6
Shulliikkk <68589028+Shulliikkk@users.noreply.github.com>
2021-01-25 00:06:30 +0300

    Stable game framework

--- RainbowLifeMain.py
+++ RainbowLifeMain.py
@@ -22,25 +22,11 @@ Arr2 = Arr1
 
 scr = pygame.display.set_mode((WIN_WIDE, WIN_HIGH))
 font = pygame.font.SysFont('couriernew', 24)
+Clok = pygame.time.Clock()
 
 col = 'Зеленый'
-def TextForSimulation():
-    global font
-    text14 = font.render('Для паузы', True, WHITE)
-    text15 = font.render("нажмите на 'p'", True, WHITE)
-    text16 = font.render('Для сброса', True, WHITE)
-    text17 = font.render("нажмите на 'f'", True, WHITE)
-    scr.blit(text14, (785, 45))
-    scr.blit(text15, (755, 70))
-    scr.blit(text16, (780, 105))
-    scr.blit(text17, (755, 130))
-
 def TextForRanking(Col):
     global font, col
-    text1 = font.render('Для заселения', True, WHITE)
-    text2 = font.render('щелкайте по клеткам', True, WHITE)
-    text3 = font.render('Текущий цвет:', True, WHITE)
-
     if Col == RED:
         pygame.draw.rect(scr, BLACK, (790, 150, 150, 26))
         col = 'Красный'
@@ -50,7 +36,9 @@ def TextForRanking(Col):
     elif Col == BLUE:
         pygame.draw.rect(scr, BLACK, (790, 150, 150, 26))
         col = 'Синий'
-
+    text1 = font.render('Для заселения', True, WHITE)
+    text2 = font.render('щелкайте по клеткам', True, WHITE)
+    text3 = font.render('Текущий цвет:', True, WHITE)
     text4 = font.render(col, True, Col)
     text5 = font.render('Для выбора цвета', True, WHITE)
     text6 = font.render('нажмите на:', True, WHITE)
@@ -61,7 +49,6 @@ def TextForRanking(Col):
     text11 = font.render("нажмите на 's'", True, WHITE)
     text12 = font.render('Для сброса', True, WHITE)
     text13 = font.render("нажмите на 'f'", True, WHITE)
-
     scr.blit(text1, (755, 45))
     scr.blit(text2, (720, 70))
     scr.blit(text3, (755, 120))
@@ -78,6 +65,17 @@ def TextForRanking(Col):
     scr.blit(text11, (760, 345))
     scr.blit(text12, (780, 380))
     scr.blit(text13, (755, 405))
+
+def TextForSimulation():
+    global font
+    text14 = font.render('Для паузы', True, WHITE)
+    text15 = font.render("нажмите на 'p'", True, WHITE)
+    text16 = font.render('Для сброса', True, WHITE)
+    text17 = font.render("нажмите на 'f'", True, WHITE)
+    scr.blit(text14, (785, 45))
+    scr.blit(text15, (755, 70))
+    scr.blit(text16, (780, 105))
+    scr.blit(text17, (755, 130))
 
 def StartFill():
     global x_surf, y_surf
@@ -112,58 +110,12 @@ def CellPaint(clik, Color):
     pygame.display.update()
 
 def Simulation():
-    N, N1, N2, N3 = 0, 0, 0, 0
-    for i in range(1, n - 1):
-        for j in range(1, n - 1):
-            if Arr1[i][j] == 0:
-                for k1 in coef:
-                    for k2 in coef:
-                        if Arr1[i + k1][j + k2] == 1:
-                            N += 1
-                            N1 += 1
-                        elif Arr1[i + k1][j + k2] == 2:
-                            N += 1
-                            N2 += 1
-                        elif Arr1[i + k1][j + k2] == 3:
-                            N += 1
-                            N3 += 1
-                if N == 3:
-                    if N1 == 2:
-                        Arr2[i][j] = 1
-                    elif N2 == 2:
-                        Arr2[i][j] = 2
-                    elif N3 == 2:
-                        Arr2[i][j] = 3
-
-            if Arr1[i][j] == 1:
-                for k1 in coef:
-                    for k2 in coef:
-                        if Arr1[i + k1][j + k2] > 0 and Arr1[i + k1][j + k2] <= 3:
-                            N += 1
-                if N <= 2:
-                    Arr2[i][j] = 1
-
-            if Arr1[i][j] == 2:
-                for k1 in coef:
-                    for k2 in coef:
-                        if Arr1[i + k1][j + k2] > 0 and Arr1[i + k1][j + k2] <= 3:
-                            N += 1
-                if N >= 3 and N <= 5:
-                    Arr2[i][j] = 2
-
-            if Arr1[i][j] == 3:
-                for k1 in coef:
-                    for k2 in coef:
-                        if Arr1[i + k1][j + k2] > 0 and Arr1[i + k1][j + k2] <= 3:
-                            N += 1
-                if N >= 6 and N <= 8:
-                    Arr2[i][j] = 3
+    pass
 
 game = True
 sim = False
 start_game = True
 ranking = True
-pause = False
 color = GREEN
 while game:
     events = pygame.event.get()
@@ -184,19 +136,19 @@ while game:
                 ranking = False
                 sim = True
             elif i.unicode == 'p':
-                pause = True
                 ranking = True
+                sim = False
         if i.type == pygame.MOUSEBUTTONDOWN:
             if i.button == 1 and ranking == True:
                 if color == RED or color == GREEN or color == BLUE:
                     CellPaint(i.pos, color)
 
-    if start_game == True:
+    if start_game:
         StartFill()
         start_game  = False
         ranking = True
 
-    if ranking == True:
+    if ranking:
         pygame.draw.rect(scr, BLACK, (x_surf + SURF_WIDE, 0, 300, WIN_HIGH))
         TextForRanking(color)
         pygame.display.update()
@@ -204,3 +156,8 @@ while game:
         pygame.draw.rect(scr, BLACK, (x_surf+SURF_WIDE, 0, 300, WIN_HIGH))
         TextForSimulation()
         pygame.display.update()
+
+    if sim:
+        Simulation()
+    Clok.tick(60)
+
